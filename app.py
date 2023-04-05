2b84a805e169a110d0faaf3c077146fc8e11dbb3
chervachek1 <kusmichandrij@gmail.com>
2023-04-05 21:45:58 +0300

    create app

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, url_for, send_file, render_template
+import os
+import time
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        start_time = time.time()
+        file = request.files['file']
+        filename = file.filename
+        file.save(os.path.join('files', filename))
+        end_time = time.time()
+        duration = round(end_time - start_time, 2)
+        file_size = os.path.getsize(os.path.join('files', filename))
+        data = {
+            'duration': duration,
+            'end_time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time)),
+            'file_size': file_size,
+            'file_link': url_for('download_file', filename=filename),
+        }
+        return render_template('download.html', data=data)
+    return render_template('upload.html')
+
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    path = f'files/{filename}'
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
